1478cc44e6d3a24ef530cb548c2bf13c1ffbdc9f
Nyud <42289762+yudhabotemz@users.noreply.github.com>
2021-09-03 23:11:29 +0700

    Update M3U8.py

--- M3U8.py
+++ M3U8.py
@@ -1,7 +1,8 @@
 # RekordBox Playlist Export into Folder
 # get music path file from *.m3u8 playlist export format and bulk copy to destination folder
 # at RekordBox app, right click on playlist you want to export -> Export a playlist to a file -> for music apps (*.m3u8)
-# run this script with Administrator level command prompt, so it can have access for copy paste to user dir
+# run this script with Administrator level command prompt, so it can have access for copy paste to user dir (not necessary if just from usual local drive to usual local drive)
+# it had shitty UI for picking a file and destination dir, but nevertheless works just fine for me
 
 import fnmatch
 import os
